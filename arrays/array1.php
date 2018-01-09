4f2afbe960938f682118464de6a480218263f224
Yasir Bhutta <yasirbhutta@gmail.com>
2018-01-09 09:41:06 +0500

    b

--- arrays/array1.php
+++ arrays/array1.php
@@ -6,11 +6,13 @@
     <body>
     <?php
 // index always starts at 0
-$cars = array("Volvo", "BMW", "Toyota");
+// $cars = array("Volvo", "BMW", "Toyota");
 
-/*$cars[0] = "Volvo";
+$cars[0] = "Volvo";
 $cars[1] = "BMW";
-$cars[2] = "Toyota";*/
+$cars[2] = "Toyota";
+
+$cars[10] = "Toyota";
 
 echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
 
@@ -24,12 +26,12 @@ echo "<br>Number of elements" . count($cars);
 // Loop through an index array
 //
 
-/*$arrlength = count($cars);
+$arrlength = count($cars);
 
 for($x = 0; $x < $arrlength; $x++) {
     echo $cars[$x];
-    echo "<br>";*/
-
+    echo "<br>";
+}
 ?>
 
 
