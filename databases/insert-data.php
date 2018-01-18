54ea8e5335347ce581a8aa5389ed7393a802cdfd
Yasir Bhutta <yasirbhutta@gmail.com>
2018-01-18 15:23:20 +0500

    dd

--- databases/insert-data.php
+++ databases/insert-data.php
@@ -3,8 +3,10 @@
 // insert date in Employees table 
 //
 
-$sql = "INSERT INTO EMPLOYEES (first_name, last_name, email, phone_number, hire_date, salary, department_ID)
-VALUES ('Muhammad', 'Ahmad', 'mahmad@example..com','0300-0000000','2016-12-19',10000,1)";
+$sql = "INSERT INTO EMPLOYEES (first_name, last_name, email, 
+phone_number, hire_date, salary, department_ID)
+VALUES ('Muhammad', 'Ahmad', 
+'mahmad@example..com','0300-0000000','2016-12-19',10000,1)";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
