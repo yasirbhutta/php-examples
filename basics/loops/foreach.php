8e8144a1705253dbbcf0ea0caf165b92f5624f67
myasir786 <myasir@gudgk.edu.pk>
2021-01-25 15:34:06 +0500

    foreach update

--- basics/loops/foreach.php
+++ basics/loops/foreach.php
@@ -4,14 +4,20 @@
         <title>PHP - foreach</title>
     </head>
     <body>
+    
+
     <?php
 /*foreach ($array as $value) {
     code to be executed;
 }*/
-$colors = array("red", "green", "blue", "yellow");
+$colors = array("red", "green", "blue", "yellow","pink");
 
-foreach ($colors as $val) {
-    echo "$val <br>";
+foreach ($colors as $i) 
+
+{
+
+echo "<h1 style=". "background-color:$i>" . $i ."</h1><br>";
+    
 }
 ?>
     </body>
