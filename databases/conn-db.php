2ba284facb1e159d54e5347d3ec6571404c0f556
Muhammad Yasir Bhutta <yasirbhutta@gmail.com>
2022-02-04 09:55:37 +0500

    db variable

--- databases/conn-db.php
+++ databases/conn-db.php
@@ -10,5 +10,5 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-echo "Connected successfully with 'hr' database<br>";
+echo "Connected successfully with $dbname database<br>";
 ?>
