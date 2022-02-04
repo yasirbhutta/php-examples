a8c689a5e2e2524dc9ccb77801526293dac867bb
Muhammad Yasir Bhutta <yasirbhutta@gmail.com>
2022-02-04 10:02:17 +0500

    update conn db

--- databases/conn-db.php
+++ databases/conn-db.php
@@ -10,5 +10,5 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-echo "Connected successfully with $dbname database<br>";
+echo "Connected successfully with <strong> $dbname </strong> database<br>";
 ?>
